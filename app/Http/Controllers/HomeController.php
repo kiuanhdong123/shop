e61f509348bb76f46534c6675a930074dcb01f4a
kiuanhdong123 <tuank454@gmail.com>
2021-01-05 15:04:34 +0700

    doi file controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,11 @@ use DB;
 use App\Http\Requests;
 use App\Models\category;
 use App\Models\brand;
+use App\Models\product;
+use App\Models\user;
+use App\Models\cart;
 use Session;
+
 
 class HomeController extends Controller
 {
@@ -23,16 +27,6 @@ class HomeController extends Controller
         $listProduct = DB::table('product')->get();
         return view("home")->with(['listCategory'=>$listCategory,'listBrand'=>$listBrand,'listProduct'=>$listProduct]);
     }
-
-    public function indexlogin()
-    {
-        return view("login")->with(['mess'=>'']);
-    }    
-
-    public function indexregister()
-    {
-        return view("register")->with(['mess'=>'']);
-    }    
 
     public function brand(Request $request)
     {
@@ -69,7 +63,13 @@ class HomeController extends Controller
         $listProduct = DB::table('product')->get();
 
         $userName = $request->session()->get('userName');
-        $listCart = DB::table('cart')->join('product', 'cart.pid', '=', 'product.id')->select('cart.id','product.pName','product.price','cart.quantity','cart.totalbill','cart.buy','cart.date','cart.status')->where('cart.userName','=',$userName)->orderBy('cart.date', 'desc')->get();
+        $permission = $request->session()->get('permission');
+        $listCart;
+        if ($permission == 0){
+            $listCart = DB::table('cart')->join('product', 'cart.pid', '=', 'product.id')->select('cart.id','product.pName','product.price','cart.quantity','cart.totalbill','cart.date','cart.status')->where('cart.status','=',1)->orderBy('cart.date', 'desc')->get();
+        }
+        else
+            $listCart = DB::table('cart')->join('product', 'cart.pid', '=', 'product.id')->select('cart.id','product.pName','product.price','cart.quantity','cart.totalbill','cart.date','cart.status')->where('cart.userName','=',$userName)->orderBy('cart.date', 'desc')->get();
         if (strlen( $listCart) != 2)
             $mess = '1';
         return view("cart")->with(['listCategory'=>$listCategory,'listBrand'=>$listBrand,'listProduct'=>$listProduct,'listCart'=>$listCart,'mess'=>$mess]);
@@ -92,63 +92,21 @@ class HomeController extends Controller
         }
         else
         if ($update == 'Buy'){
-            DB::table('cart')->where('id', $id)->update(['status' => 1,'buy' => 1]);
+            DB::table('cart')->where('id', $id)->update(['status' => 1]);
         }
         else
         if ($update == 'New'){
             $userName = $request->session()->get('userName');
             $pid = $request->pid;
             $quantity = $request->quantity;
-            DB::table('cart')->insert(['userName' => $userName, 'pid' => $pid, 'quantity' => $quantity, 'totalbill' => ($quantity*$price), 'buy' => 0, 'status' => 0, 'date' => NOW()]);
+            DB::table('cart')->insert(['userName' => $userName, 'pid' => $pid, 'quantity' => $quantity, 'totalbill' => ($quantity*$price), 'status' => 0, 'date' => NOW()]);
+        }
+        else
+        if ($update == 'Check'){
+            DB::table('cart')->where('id', $id)->update(['status' => 2]);
         }
         DB::table('cart')->where('quantity',0)->delete();
         return redirect()->route('cart');
-    }
-
-    public function logincontroller(Request $request)
-    {
-        $mess = '';
-        $username = $request->username;
-        $password = $request->password;
-
-        $account = DB::table('user')->where('userName',$username)->where('password',$password)->get();
-
-        if (strlen( $account) != 2){
-            // Request::session()->put('Alogin', true);
-            // Request::session()->put('Uname', $username);
-            
-            $request->session()->put('userName', $account->get(0)->userName);
-            $request->session()->put('permission', $account->get(0)->permission);
-
-            //$test = $request->session()->get('Uname');
-         
-            return redirect()->route('home');
-        }else{
-            return view("login")->with(['mess'=>'Account has not already exists']);
-        }
-        return view("login")->with(['account'=>$account]);
-    }
-
-    public function registercontroller(Request $request)
-    {
-        $id = $request->id;
-        $mess = '';
-        $username = $request->username;
-        $password = $request->password;
-        $email = $request->email;
-        $address = $request->address;
-        $phone = $request->phone;
-
-        $account = DB::table('user')->where('userName',$username)->get();
-        if (strlen( $account) != 2){
-            return view("register")->with(['mess'=>'Username has already exists']);
-        }else{
-            $data=array('userName'=>$username,'password'=>$password,'email'=>$email,'permission'=>1,'address'=>$address,'phone'=>$phone);
-            DB::table('user')->insert($data);
-            return view("register")->with(['mess'=>'Username has created successfully']);
-        }
-        
-        return view("register")->with(['mess'=>$mess]);
     }
 
     /**
